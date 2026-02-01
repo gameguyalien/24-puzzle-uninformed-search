301c5f9ae0bdaef4a9bf4462fdf51e5cbbf5c97e
Ryan Thomson <gameguyalien@gmail.com>
2026-02-01 15:12:49 -0500

    adding dfs search
    Signed-off-by: Ryan Thomson <gameguyalien@gmail.com>

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -8,8 +8,14 @@ import (
 
 type Board []uint8
 
+type DFSNode struct {
+	board Board
+	depth int
+}
+
 func loadBoard(bs int) Board {
-	if bs == 5 {
+	switch bs {
+	case 5:
 		return Board{
 			9, 24, 3, 5, 17,
 			6, 0, 13, 19, 10,
@@ -17,20 +23,20 @@ func loadBoard(bs int) Board {
 			16, 4, 14, 12, 15,
 			8, 18, 23, 2, 7,
 		}
-	} else if bs == 4 {
+	case 4:
 		return Board{
 			1, 2, 7, 4,
 			5, 15, 0, 8,
 			9, 10, 3, 11,
 			13, 14, 6, 12,
 		}
-	} else if bs == 3 {
+	case 3:
 		return Board{
 			1, 2, 3,
-			4, 0, 5,
-			7, 8, 6,
+			8, 0, 5,
+			7, 6, 4,
 		}
-	} else {
+	default:
 		panic("Unsupported board size")
 	}
 }
@@ -127,29 +133,107 @@ func bfs(start Board, goal Board, bs int) ([]Board, bool) {
 	return nil, false
 }
 
+func dfs(start Board, goal Board, bs int, maxDepth int) ([]Board, bool) {
+	stack := []DFSNode{{board: start, depth: 0}}
+	visited := make(map[string]bool)
+	parent := make(map[string]Board)
+
+	visited[boardKey(start)] = true
+
+	nodesExplored := 0
+
+	for len(stack) > 0 {
+		n := stack[len(stack)-1]
+		stack = stack[:len(stack)-1]
+
+		current := n.board
+		depth := n.depth
+		nodesExplored++
+
+		if boardKey(current) == boardKey(goal) {
+			// reconstruct path
+			var path []Board
+			for boardKey(current) != boardKey(start) {
+				path = append([]Board{current}, path...)
+				current = parent[boardKey(current)]
+			}
+			path = append([]Board{start}, path...)
+			fmt.Printf("DFS total nodes explored: %d\n", nodesExplored)
+			return path, true
+		}
+
+		if depth >= maxDepth {
+			continue
+		}
+
+		for _, next := range successors(current, bs) {
+			key := boardKey(next)
+			if !visited[key] {
+				visited[key] = true
+				parent[key] = current
+				stack = append(stack, DFSNode{
+					board: next,
+					depth: depth + 1,
+				})
+				if nodesExplored%1000000 == 0 {
+					fmt.Printf("Nodes explored: %d\r", nodesExplored)
+				}
+			}
+		}
+	}
+
+	fmt.Printf("DFS total nodes explored: %d\n", nodesExplored)
+	return nil, false
+}
+
 func main() {
 	boardSize := flag.Int("s", 3, "Size of the board")
+	searchMethod := flag.String("m", "bfs", "Search method: bfs")
+	dfsDepth := flag.Int("d", 30, "Max depth for DFS")
+
 	flag.Parse()
 	startTime := time.Now()
 	board := loadBoard(*boardSize)
 	goal := goalBoard(*boardSize)
-	path, found := bfs(board, goal, *boardSize)
-	if found {
-		fmt.Printf("Solution Found! Path length: %d\n", len(path))
-		fmt.Printf("Time taken: %s\n", time.Since(startTime))
-		fmt.Printf("Time Started: %s\nTime Ended: %s\n", startTime.Format(time.RFC3339), time.Now().Format(time.RFC3339))
-		for i, step := range path {
-			fmt.Printf("Step %d:\n", i)
-			for r := 0; r < *boardSize; r++ {
-				for c := 0; c < *boardSize; c++ {
-					fmt.Printf("%d ", step[r**boardSize+c])
+	switch *searchMethod {
+	case "bfs":
+		path, found := bfs(board, goal, *boardSize)
+		if found {
+			fmt.Printf("BFS Solution Found! Path length: %d\n", len(path))
+			fmt.Printf("Time taken: %s\n", time.Since(startTime))
+			fmt.Printf("Time Started: %s\nTime Ended: %s\n", startTime.Format(time.RFC3339), time.Now().Format(time.RFC3339))
+			for i, step := range path {
+				fmt.Printf("Step %d:\n", i)
+				for r := 0; r < *boardSize; r++ {
+					for c := 0; c < *boardSize; c++ {
+						fmt.Printf("%d ", step[r**boardSize+c])
+					}
+					fmt.Println()
 				}
 				fmt.Println()
 			}
-			fmt.Println()
 		}
-	} else {
-		println("No solution found.")
-	}
+	case "dfs":
+		path, found := dfs(board, goal, *boardSize, *dfsDepth)
 
+		if found {
+			fmt.Printf("DFS Solution Found! Path length: %d\n", len(path))
+			fmt.Printf("Time taken: %s\n", time.Since(startTime))
+			fmt.Printf("Time Started: %s\nTime Ended: %s\n", startTime.Format(time.RFC3339), time.Now().Format(time.RFC3339))
+			for i, step := range path {
+				fmt.Printf("Step %d:\n", i)
+				for r := 0; r < *boardSize; r++ {
+					for c := 0; c < *boardSize; c++ {
+						fmt.Printf("%d ", step[r**boardSize+c])
+					}
+					fmt.Println()
+				}
+				fmt.Println()
+			}
+		} else {
+			fmt.Println("DFS: No solution within depth limit")
+		}
+	default:
+		println("Invalid search method. Use 'bfs' or 'dfs'.")
+	}
 }
